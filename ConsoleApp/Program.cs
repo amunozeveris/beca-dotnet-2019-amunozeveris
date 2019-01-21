1aac23e3be52b553e6246fa7cfcb0989dc6698ac
amunozgarcia <alexmg.alt@gmail.com>
2019-01-21 16:32:53 +0100

    Hola Mundo

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola Mundo!");
+            Console.ReadLine();
+            
         }
     }
 }
