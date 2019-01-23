f2b79a0a38a05a989646008b461c4be9477622c5
amunozgarcia <alexmg.alt@gmail.com>
2019-01-23 15:19:39 +0100

    Repair BodyWork class

---
+++ BusinessCore/Bodywork.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCore
+{
+    public class Bodywork
+    {
+        public string Brand { get; set; }
+
+        public Bodywork(string brand)
+        {
+            this.Brand = brand;
+        }
+    }
+}
