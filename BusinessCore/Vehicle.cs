d51615c4dbd7feb799c06d1b05bd6429f31cd2fe
amunozgarcia <alexmg.alt@gmail.com>
2019-01-23 12:43:57 +0100

    Add Vehicle class

---
+++ BusinessCore/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCore
+{
+    public abstract class Vehicle
+    {
+        public string brand;
+        public Bodywork bodywork;
+        public Chassis chassis;
+        public Wheel[] wheels;
+        public Door[] doors;
+
+     
+        public Vehicle(string brand, Bodywork bodywork, Chassis chassis, Wheel[] wheels, Door[] doors)
+        {
+            this.brand = brand;
+            this.bodywork = bodywork;
+            this.chassis = chassis;
+            this.wheels = wheels;
+            this.doors = doors;
+        }
+    }
+}
