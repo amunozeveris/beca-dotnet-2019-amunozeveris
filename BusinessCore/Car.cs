ae0a8eaa29c4871cb2ed3972547fbe2a91e6e363
amunozgarcia <alexmg.alt@gmail.com>
2019-01-23 12:46:49 +0100

    Add Vehicle car

---
+++ BusinessCore/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCore
+{
+    public class Car : Vehicle
+    {
+
+        public Car(string brand, Bodywork bodywork, Chassis chassis, Wheel[] wheels, Door[] doors)
+             : base(brand, bodywork, chassis, wheels, doors)
+        {
+        }
+    }
+}
