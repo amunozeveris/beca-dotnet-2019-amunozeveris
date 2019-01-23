784287cac44c5f39b34b0c0f1db89093e16986ae
amunozgarcia <alexmg.alt@gmail.com>
2019-01-23 12:46:38 +0100

    Add Vehicle parts2

---
+++ BusinessCore/Wheel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessCore
+{
+    public class Wheel
+    {
+        public string Brand { get; set; }
+        public float Inches { get; set; }
+
+        public Wheel(string brand, float inches)
+        {
+            this.Brand = brand;
+            this.Inches = inches;
+        }
+    }
+}
